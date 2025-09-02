1cdcf515eb1a46e31cec66a33229941acc93192a
pepe <pepe.grz@googlemail.com>
2025-09-02 13:23:55 +0200

    created data_models

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from data_models import db
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Link database and app
+db.init_app(app)
+
+print("TEST")
+
+with app.app_context():
+    db.create_all()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5002, debug=True)
---
+++ data_models.py
@@ -0,0 +1,61 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    native_language = db.Column(db.String(5), nullable=False)
+    created_at = db.Column(db.Date)
+
+
+
+class User_Languages(db.Model):
+    __tablename__ = 'user_languages'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    language_code = db.Column(db.String(5), nullable=False)
+    created_at = db.Column(db.Date)
+
+
+class Sentences(db.Model):
+    __tablename__ = 'sentences'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    original_text = db.Column(db.String(200), nullable=False)
+    language_code = db.Column(db.String(5), nullable=False)
+    category = db.Column(db.String(50))
+    created_at = db.Column(db.Date)
+
+
+class Translations(db.Model):
+    __tablename__ = 'translations'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    sentence_id = db.Column(db.Integer, db.ForeignKey('sentences.id'), nullable=False)
+    translated_text = db.Column(db.String(200))
+    target_language_code = db.Column(db.String(5))
+    created_at = db.Column(db.Date)
+
+
+class Learning_Progress(db.Model):
+    __tablename__ = 'learning_progress'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    translation_id = db.Column(db.Integer, db.ForeignKey('translations.id'), nullable=False)
+    score = db.Column(db.Integer, default=0)
+    last_reviewed = db.Column(db.Date)
+    next_review = db.Column(db.Date)
+    review_count = db.Column(db.Integer, default=0)
+    success_rate = db.Column(db.Integer, default=0)
+    # every user has for every translation only one progressdataset
+    __table_args__ = (db.UniqueConstraint('user_id', 'translation_id'),)
+
+
+
