b7acc587da26b9af6abfccb19a11ae9ef21b2cfa
pepe <pepe@localhost.localdomain>
2025-09-04 16:01:39 +0200

    added data_manager.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from data_models import db
-
+from data_manager import DataManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -16,6 +16,7 @@ print("TEST")
 with app.app_context():
     db.create_all()
 
+manager = DataManager(db)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
